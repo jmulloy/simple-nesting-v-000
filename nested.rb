9bb0b1d9ad383bec52c5f3aceb019efd8b2ded88
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-19 23:02:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -101,10 +101,15 @@ def changing_alan
           :languages => ["C"]
         }
      }
+
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-
+		 programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
 end
 
 def adding_to_dennis
