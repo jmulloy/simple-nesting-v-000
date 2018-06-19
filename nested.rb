63353e7e72e26985617199c2992519e150c03900
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-19 23:04:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-
+		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
@@ -103,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
 
@@ -123,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
